c1525196172ac9da2693c8c497ab67a28e88558b
Deyvis Charles <davis.superheavy@gmail.com>
2020-10-18 20:43:15 -0300

    Refactor: Adicionando variável de ambiente.

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/xmachine", {
+mongoose.connect(process.env.MDB_URL, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true
