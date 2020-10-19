2a588c35114fab490e19cfca7ed49b2969204a91
Deyvis Charles <davis.superheavy@gmail.com>
2020-10-18 21:03:46 -0300

    Change: Mudança na mensagem de boas-vindas.

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,7 +4,7 @@ const route = express.Router()
 
 route.get('/', async (req, res) => {
 	res.send({
-		message: 'Bem-vindo(a) a Api da Supermachine'
+		message: 'Bem-vindo(a) a Api Xmachine (by Deyvis Charles)'
 	})
 })
 
